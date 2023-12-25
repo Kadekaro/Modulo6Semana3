e8373048b7901bdb3e6cfc59930e247a1fe67b35
kadekaro <kadekaroshop@gmail.com>
2023-12-25 01:10:39 -0300

    first commit

---
+++ Base/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from Base.models import Contato
+
+
+class contatoForms(forms.ModelForm):
+    class Meta:
+        model = Contato
+        fields = ['nome', 'email', 'mensagem']
+
